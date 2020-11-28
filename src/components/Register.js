b2316b06f9aec9aa55e068c50457aac6255e227e
Salem-Abderaouf <salamasalem84@gmail.com>
2020-11-28 21:24:47 +0100

    Create and update sign-up file

---
+++ src/components/Register.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from './Nav'
+import LeftSide from './LeftSide'
+
+
+const Register = () => {
+  return (
+    <>
+      <div className="row">
+        <LeftSide />
+      </div>
+    </>
+  )
+}
+export default Register;
