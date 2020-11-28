29fac939f150417ce841e59dc1546bc8fcd10e67
Salem-Abderaouf <salamasalem84@gmail.com>
2020-11-28 21:25:13 +0100

    Create and update LeftSide component

---
+++ src/components/LeftSide.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LeftSide = () => {
+  return (
+    <div className="d-md-flex d-lg-flex d-sm-none vh-100 left p-3">
+      <h3 className="h-brand">Weecho</h3>
+    </div>
+  )
+}
+export default LeftSide;
