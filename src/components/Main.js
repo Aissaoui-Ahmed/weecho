e2482720c7898cfdbbea9287d44fd79318c3019c
Salem-Abderaouf <salamasalem84@gmail.com>
2020-11-30 20:53:09 +0100

    update Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,12 +6,12 @@ const Main = () => {
     <main className="row justify-content-between align-items-center" id="main">
       <div className="col-lg-6 col-md-6 col-sm-12 text-center mt-5">
         <h1>Build communications with your consumers
-          easily on <span id="we-brand">weecho</span>.</h1>
+          easily on <span id="we-brand">weecho.</span></h1>
         <p className=" text-center">
           Weecho platform for handling <br></br> many users in each time
         </p>
-        <Link className="btn btn-register py-3 px-3" to="/register">Register</Link>
-        <Link className="btn btn-learn-more py-3 px-3">learn More</Link>
+        <Link className="btn btn-register mx-2 py-3 px-3" to="/register">Register</Link>
+        <Link className="btn btn-learn-more py-3 px-3" to="/learn-more">learn More</Link>
       </div>
       <div className="col-lg-6 col-md-6 col-sm-12" id="work">
       </div>
