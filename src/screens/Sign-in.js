07e4ec907c6f2d47eba990d1b7aa8db1ee4d23b2
Salem-Abderaouf <salamasalem84@gmail.com>
2020-11-30 20:50:57 +0100

    create sign in screen

---
+++ src/screens/Sign-in.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import LeftSide from '../components/LeftSide'
+import Signin from '../components/Signin'
+
+const Register = () => {
+  return (
+    <>
+      <div className="row">
+        <LeftSide />
+        <Signin />
+      </div>
+    </>
+  )
+}
+export default Register;
