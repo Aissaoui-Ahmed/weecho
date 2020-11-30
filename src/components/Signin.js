506399a103e48cdbdbfdfdcdfa7b19978a632630
Salem-Abderaouf <salamasalem84@gmail.com>
2020-12-01 00:58:57 +0100

    update sign-in

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,19 +1,43 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import db from '../config/db'
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [passwd, setPasswd] = useState('')
+  const [isProg, setIsProg] = useState(false)
   const [err, setErr] = useState(null)
 
   const handleSubmit = (event) => {
-    const form = document.getElementById('mrof')
     event.preventDefault()
-    if (email !== '' && passwd !== '') {
+    if (email === '' && passwd === '') {
       setErr('Fill the inputs ')
     }
     else {
-      form.submit()
+      setIsProg(true)
+      db.auth()
+        .signInWithEmailAndPassword(email, passwd)
+        .then((res) => {
+          const uid = res.user.uid;
+          const usersRef = db.database().ref(`u/${uid}`);
+          usersRef
+            .once('value')
+            .then((snapShot) => {
+              if (!snapShot.exists) {
+                setErr('User does not exist !');
+                return;
+              }
+              setIsProg(false);
+            })
+            .catch((err) => {
+              setIsProg(false)
+              setErr(err.message);
+            });
+        })
+        .catch((err) => {
+          setIsProg(false)
+          setErr(err.message);
+        });
     }
 
   }
@@ -55,7 +79,53 @@ const Login = () => {
               </div>
 
               <div className="input-group px-sm-1 px-md-3 my-2">
-                <input type="submit" onClick={handleSubmit} className="btn py-sm-3 py-md-3 register-btn mx-2" value="LOGIN" />
+                {isProg ?
+                  <div className="svgLoading py-sm-2 py-md-2 mx-2">
+                    <svg width="45" height="45" viewBox="0 0 45 45" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
+                      <g fill="none" fill-rule="evenodd" transform="translate(1 1)" stroke-width="2">
+                        <circle cx="22" cy="22" r="6" stroke-opacity="0">
+                          <animate attributeName="r"
+                            begin="1.5s" dur="3s"
+                            values="6;22"
+                            calcMode="linear"
+                            repeatCount="indefinite" />
+                          <animate attributeName="stroke-opacity"
+                            begin="1.5s" dur="3s"
+                            values="1;0" calcMode="linear"
+                            repeatCount="indefinite" />
+                          <animate attributeName="stroke-width"
+                            begin="1.5s" dur="3s"
+                            values="2;0" calcMode="linear"
+                            repeatCount="indefinite" />
+                        </circle>
+                        <circle cx="22" cy="22" r="6" stroke-opacity="0">
+                          <animate attributeName="r"
+                            begin="3s" dur="3s"
+                            values="6;22"
+                            calcMode="linear"
+                            repeatCount="indefinite" />
+                          <animate attributeName="stroke-opacity"
+                            begin="3s" dur="3s"
+                            values="1;0" calcMode="linear"
+                            repeatCount="indefinite" />
+                          <animate attributeName="stroke-width"
+                            begin="3s" dur="3s"
+                            values="2;0" calcMode="linear"
+                            repeatCount="indefinite" />
+                        </circle>
+                        <circle cx="22" cy="22" r="8">
+                          <animate attributeName="r"
+                            begin="0s" dur="1.5s"
+                            values="6;1;2;3;4;5;6"
+                            calcMode="linear"
+                            repeatCount="indefinite" />
+                        </circle>
+                      </g>
+                    </svg>
+                  </div>
+                  :
+                  <input type="submit" onClick={handleSubmit} className="btn py-sm-3 py-md-3 register-btn mx-2" value="LOGIN" />
+                }
               </div>
             </div>
           </form>
