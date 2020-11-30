63200936b9d66011586501755f9943dafb6a3f7d
Salem-Abderaouf <salamasalem84@gmail.com>
2020-11-30 20:05:59 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './static/App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from './components/Home'
+import Hero from './screens/Hero'
 import NotFound from './components/NotFound'
-import Register from './components/Register'
+import Register from './screens/Sign-up'
+import Login from './screens/Sign-in'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Hero />
         </Route>
         <Route exact path="/register">
           <Register />
+        </Route>
+        <Route exact path="/login">
+          <Login />
         </Route>
         <Route path="*">
           <NotFound />
