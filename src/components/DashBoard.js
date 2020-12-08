7f2b19d853842e280f74c8b6d549c63bc7408fdd
Salem-Abderaouf <salamasalem84@gmail.com>
2020-12-08 21:57:54 +0100

    update main Dashboard

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DashBoard = () => {
-  const [body, setBody] = React.useState('')
-  const [num, setNumber] = React.useState(0)
-  const [err, setErr] = React.useState(null)
+  const [body, setBody] = useState('')
+  const [num, setNumber] = useState(0)
+  const [err, setErr] = useState(null)
+
   const SendMessage = (event) => {
     event.preventDefault()
     if (num === '' || body === '') {
@@ -11,33 +12,42 @@ const DashBoard = () => {
     }
     else { fetch() }
   }
+  const uploadFile = (eve) => {
+    const file = document.getElementById('file')
+    file.click()
+  }
   return (
     <div className="row">
       <div className="col-12 m-3 p-4 quick-sender">
-        <h4 className="mb-3 text-center">Fast send</h4>
         <div className="px-sm-2 px-md-4">
-          <div className={err == null ? "d-none" : "alert alert-danger "} role="alert">
+          <div className={err == null ? "d-none" : "alert alert-danger"} role="alert">
             {err}
           </div>
         </div>
         <form>
           <div className="input-group px-sm-2 px-md-4 my-2 pinned">
             <textarea
-              className="form-control my-2 mr-2 py-2 input"
+              className="form-control my-2 mr-2 py-2 input qs-input"
               onChange={(val) => setBody(val)}
               placeholder='Write down message to send'></textarea>
             <input
               onChange={(val) => setNumber(val)}
-              className="form-control my-2  py-4 input"
+              className="form-control my-2  py-4 input qs-input"
               type="tel" placeholder="Phone number" />
+            <br></br>
             <button
               onClick={SendMessage}
-              className="send-button py-sm-3 py-md-3">Send</button>
+              className="send-button py-sm-2 py-md-2">SEND</button>
           </div>
         </form>
       </div>
-      <div className="col-6">data</div>
-      <div className="col-6">state</div>
+      <div className="col-4 file-devision py-4 m-3">
+        <button onClick={uploadFile} className="custom-file-upload" >
+          Upload *.json <br></br> or *.exel
+        </button>
+        <input type="file" id="file" />
+      </div>
+      <div className="col-8"></div>
     </div>
   )
 }
