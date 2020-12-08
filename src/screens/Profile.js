6cc8a5f53022dbbf787531569b5dbe042474907e
Salem-Abderaouf <salamasalem84@gmail.com>
2020-12-08 21:56:39 +0100

    Add Profile screen

---
+++ src/screens/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from '../components/Sidebar'
+import ProfileInformation from '../components/ProfileInformation'
+
+const Profile = () => {
+  return (
+    <>
+      <SideBar />
+      <ProfileInformation />
+    </>
+  )
+}
+export default Profile;
